b4f15dd6559ccabb4fb2b4d7f1685a1c5fda2cd6
Matheus Richard <matheusrichardt@gmail.com>
2022-01-15 18:44:32 -0300

    Test ruby version parser with Gemfile.lock

--- spec/eol_ruby/ruby_version/parser_spec.rb
+++ spec/eol_ruby/ruby_version/parser_spec.rb
@@ -4,6 +4,65 @@ require "spec_helper"
 
 RSpec.describe EolRuby::RubyVersion::Parser do
   describe ".parse_file" do
+    context "with .ruby-version" do
+      it "returns ruby version defined" do
+        result = described_class.parse_file(file_name: ".ruby-version", content: "3.0.0")
+
+        expect(result).to eq EolRuby::RubyVersion.new("3.0.0")
+      end
+
+      it "removes the 'ruby-' prefix" do
+        result = described_class.parse_file(file_name: ".ruby-version", content: "ruby-2.0.0\n")
+
+        expect(result).to eq EolRuby::RubyVersion.new("2.0.0")
+      end
+    end
+
+    context "with Gemfile.lock" do
+      it "returns the ruby version defined" do
+        gemfile_lock = <<~GEMFILE_LOCK
+          GEM
+            remote: https://rubygems.org/
+            specs:
+
+          PLATFORMS
+            x86_64-darwin-20
+
+          DEPENDENCIES
+
+          RUBY VERSION
+            ruby 3.0.2p107
+
+          BUNDLED WITH
+            2.3.4
+        GEMFILE_LOCK
+
+        result = described_class.parse_file(file_name: "Gemfile.lock", content: gemfile_lock)
+
+        expect(result).to eq EolRuby::RubyVersion.new("3.0.2p107")
+      end
+
+      it "returns nil if it doesn't have ruby version defined" do
+        gemfile_lock = <<~GEMFILE_LOCK
+          GEM
+            remote: https://rubygems.org/
+            specs:
+
+          PLATFORMS
+            x86_64-darwin-20
+
+          DEPENDENCIES
+
+          BUNDLED WITH
+            2.3.4
+        GEMFILE_LOCK
+
+        result = described_class.parse_file(file_name: "Gemfile.lock", content: gemfile_lock)
+
+        expect(result).to be_nil
+      end
+    end
+
     context "with .tool-versions" do
       it "returns the first ruby version defined" do
         result = described_class.parse_file(file_name: ".tool-versions", content: "  ruby 3.0.0\n ruby 2.5.1")
