67de8591a8eea39cea1c990fdd6cea0528ca2dc1
Matheus Richard <matheusrichardt@gmail.com>
2022-01-15 23:18:17 -0300

    Remove unecessary code
    I forgot to delete this when I created the Parser module 😅

--- lib/end_of_life/ruby_version.rb
+++ lib/end_of_life/ruby_version.rb
@@ -14,44 +14,6 @@ module EndOfLife
       Parser.parse_file(file_name: file_name, content: content)
     end
 
-    def self.parse_ruby_version_file(file_content)
-      string_version = file_content.strip.delete_prefix("ruby-")
-
-      RubyVersion.new(string_version)
-    end
-
-    def self.parse_gemfile_lock_file(file_content)
-      gemfile_lock_version = Bundler::LockfileParser.new(file_content).ruby_version
-      return if gemfile_lock_version.nil?
-
-      RubyVersion.new(gemfile_lock_version.delete_prefix("ruby "))
-    end
-
-    def self.parse_gemfile_file(file_content)
-      return if file_content.empty?
-
-      with_temp_gemfile(file_content) do |temp_gemfile|
-        return nil if temp_gemfile.nil?
-
-        gemfile_version = temp_gemfile.ruby_version&.gem_version
-        return nil if gemfile_version.nil?
-
-        RubyVersion.new(gemfile_version)
-      end
-    end
-
-    def self.with_temp_gemfile(contents)
-      Tempfile.create("tempGemfile") do |tempfile|
-        tempfile.write(contents)
-        tempfile.rewind
-        gemfile = Bundler::Definition.build(tempfile.path, nil, {})
-
-        yield(gemfile)
-      rescue Bundler::BundlerError
-        nil
-      end
-    end
-
     attr :version
 
     def initialize(version_string)
