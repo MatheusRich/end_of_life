4d067552b3551c7f5112114b13c9440893c051ea
Duncan Bayne <duncan@bayne.id.au>
2022-02-05 09:53:46 +1100

    Fix integration spec by setting GITHUB_TOKEN
    Note that I'm setting this to the REDACTED value expected by config.
    
    Also, this used to work locally - I must have accidentally killed the
    `with_env` line while rebasing against main.

--- spec/end_of_life/repository_integration_spec.rb
+++ spec/end_of_life/repository_integration_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe EndOfLife::Repository do
   describe "#fetch" do
     it "fetches all 200 repositories from an account despite exceeding page size" do
       repositories = VCR.use_cassette("many_repositories") do
-        EndOfLife::Repository.fetch(language: "ruby", user: nil, organizations: nil, repository: nil)
+        with_env GITHUB_TOKEN: "REDACTED" do
+          EndOfLife::Repository.fetch(language: "ruby", user: nil, organizations: nil, repository: nil)
+        end
       end
       expect(repositories.value!.count).to eq(200)
     end
