5b18f4a44f6036387dba34cc14f3e917855c1009
Juan Vasquez <juan@logicalbricks.com>
2022-03-21 14:13:28 -0600

    Refactoring test

--- spec/end_of_life/cli_spec.rb
+++ spec/end_of_life/cli_spec.rb
@@ -1,27 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.describe EndOfLife::CLI do
+  subject(:cli) { described_class.new }
+
   describe "#call" do
     context "with version option" do
       it "prints the version" do
-        cli = EndOfLife::CLI.new
-
         expect { cli.call(["-v"]) }.to output("end_of_life v#{EndOfLife::VERSION}\n").to_stdout
       end
     end
 
     context "with help option" do
       it "prints the help banner" do
-        cli = EndOfLife::CLI.new
-
         expect { cli.call(["-h"]) }.to output(/Usage: end_of_life \[options\]/).to_stdout
       end
     end
 
     context "with invalid option" do
       it "exits with error message" do
-        cli = EndOfLife::CLI.new
-
         expect { cli.call(["--unknown-option"]) }
           .to exit_with_code(-1)
           .and output(/invalid option: --unknown-option/).to_stdout
